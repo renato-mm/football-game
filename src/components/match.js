a3d0ffb65ade23cf670b16675538dbf1a4a3da3d
Renato Menezes Machado <renatormm@hotmail.com>
2020-03-18 17:41:18 -0300

    Some more modifications to the in-match team screen

--- src/components/match.js
+++ src/components/match.js
@@ -8,20 +8,20 @@ import * as Teams from './teams';
 
 const attendance = 18500;
 const history = [
-  {time: 9,  stat:'G',    team:'home', player:'Éverton Ribeiro'},
-  {time: 15, stat:'CA',   team:'home', player:'Henrique'},
-  {time: 16, stat:'CA',   team:'home', player:'Éverton Ribeiro'},
-  {time: 25, stat:'CA',   team:'away', player:'Pierre'},
-  {time: 28, stat:'G',    team:'home', player:'Henrique'},
-  {time: 33, stat:'G',    team:'home', player:'Marcelo Moreno'},
-  {time: 35, stat:'CA',   team:'away', player:'Júnior César'},
-  {time: 44, stat:'CA',   team:'home', player:'Egídio'},
-  {time: 45, stat:'G',    team:'home', player:'Lucas Silva'},
-  {time: 56, stat:'G',    team:'home', player:'Ricardo Goulart'},
-  {time: 60, stat:'G',    team:'away', player:'Réver'},
-  {time: 77, stat:'CV',   team:'away', player:'Leonardo Silva'},
-  {time: 77, stat:'CV',   team:'home', player:'Ricardo Goulart'},
-  {time: 89, stat:'G',    team:'home', player:'Willian'}
+  {time: 9,  stat:'G',    team:'home', playerID: '2108', player:'Éverton Ribeiro'},
+  {time: 15, stat:'CA',   team:'home', playerID: '2109', player:'Henrique'},
+  {time: 16, stat:'CA',   team:'home', playerID: '2108', player:'Éverton Ribeiro'},
+  {time: 25, stat:'CA',   team:'away', playerID: '0609', player:'Pierre'},
+  {time: 28, stat:'G',    team:'home', playerID: '2108', player:'Henrique'},
+  {time: 33, stat:'G',    team:'home', playerID: '2113', player:'Marcelo Moreno'},
+  {time: 35, stat:'CA',   team:'away', playerID: '0602', player:'Júnior César'},
+  {time: 44, stat:'CA',   team:'home', playerID: '2104', player:'Egídio'},
+  {time: 45, stat:'G',    team:'home', playerID: '2110', player:'Lucas Silva'},
+  {time: 56, stat:'G',    team:'home', playerID: '2115', player:'Ricardo Goulart'},
+  {time: 60, stat:'G',    team:'away', playerID: '0606', player:'Réver'},
+  {time: 77, stat:'CV',   team:'away', playerID: '0603', player:'Leonardo Silva'},
+  {time: 77, stat:'CV',   team:'home', playerID: '2115', player:'Ricardo Goulart'},
+  {time: 89, stat:'G',    team:'home', playerID: '2116', player:'Willian'}
 ]
 
 function TeamModal(props) {
@@ -42,35 +42,51 @@ function TeamModal(props) {
     const plys = teamPlys.filter(e=>e.position === pos[0]);
     plys.sort((e1,e2)=>e1.power<e2.power);
     for(let j = 0; j < pos[1]; j++){
-      players.push(<tr>{plys[j].position}&nbsp;&nbsp;{plys[j].name}&nbsp;{plys[j].power}</tr>);
+      players.push(
+        <tr>
+          <td>{plys[j].position}</td>
+          <td>&nbsp;{plys[j].name}&nbsp;</td>
+          <td>{plys[j].power}</td>
+        </tr>);
     }
     for(let j = pos[1]; j < plys.length; j++){
-      reserves.push(<tr>{plys[j].position}&nbsp;&nbsp;{plys[j].name}&nbsp;{plys[j].power}</tr>);
+      reserves.push(
+        <tr>
+          <td>{plys[j].position}</td>
+          <td>&nbsp;{plys[j].name}&nbsp;</td>
+          <td>{plys[j].power}</td>
+        </tr>);
     }
   });
 
   const hist = []
   for(let j = 0; j < history.length; j++){
-    hist.push(<tr>{history[j].time}'&nbsp;&nbsp;{history[j].stat}:&nbsp;{history[j].player}</tr>);
+    hist.push(
+      <tr>
+        <td>{history[j].time.toString().padStart(2, '0')}'</td>
+        <td>&nbsp;{history[j].stat}:&nbsp;</td>
+        <td>{history[j].player}</td>
+      </tr>);
   }
 
   return (
     <div
       style = {colors}
       className = {"teamModal"}
-      onClick = { props.close }
     >
+      <button class = {"teamModalCloseButton"} onClick = { props.close }>X</button>
       <table class = {"teamModalHistoryTable"}>
         {hist}
       </table>
       <b>{props.team.name}</b>
       <div class = {"teamModalPlayers"}>
-        <table>
+        <table class = {"teamModalPlayersTable"}>
           {players}
         </table>
-        <table>
+        <table class = {"teamModalReservesTable"}>
           {reserves}
         </table>
+        <button class = {"teamModalSubButton"}><b>Substitute</b></button>
       </div>
     </div>
   );
