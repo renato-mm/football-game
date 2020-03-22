ba9d49e910d47694d157f403df2f882df272e672
Renato Menezes Machado <renatormm@hotmail.com>
2020-03-22 17:51:01 -0300

    Created index for github Pages

---
+++ src/components/matchFunctions.js
@@ -0,0 +1,9 @@
+
+// Create a function that receives two list of players as parameters
+// The function must alter the history, homeScore and awayScore states of the match object it belongs to
+// 'match.js' has a history example on line 22
+// 'teams.js' has two samples of players list, but the function only receives the ones that are currently playing
+
+function runMatch(homePlayers, awayPlayers){
+  //
+}
