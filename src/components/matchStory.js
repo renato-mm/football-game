d260f6b9e847e36ad8cbe9c134a9b5844724eac4
du3 <dudsmm@gmail.com>
2020-04-07 01:29:33 -0300

    runmatch adjustments, modularization pruning, some accent fixing

--- src/components/matchStory.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './matchStory.css';
-
-export default function MatchStory(props) {
-  return (
-    <div className = {"matchStory"} > {props.text} </div>
-  );
-}
-
