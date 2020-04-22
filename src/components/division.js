af7aec03a2df7e8ee3fea654e9a2f75815cb0b28
du3 <dudsmm@gmail.com>
2020-04-22 05:15:48 -0300

    Started work on player switch screen

--- src/components/division.js
+++ src/components/division.js
@@ -13,6 +13,7 @@ export default class Division extends React.Component {
       matchTime: 0,
       matchHalf: 1,
       screen: "Matches",
+      switchChoices: [0, 0],
     };
     this.handler = props.handler;
     this.buttonText = ["Start", "Playing", "Next Matches"]
@@ -111,12 +112,8 @@ export default class Division extends React.Component {
         </div>
       )
   }
-  
-  render(){
-    let colors = {
-      background: this.props.buttonColors[0],
-      color: this.props.buttonColors[1],
-    }
+
+  renderMatches() {
     let season = this.props.handler.get("Season", 0, "year")
     let day = this.props.handler.get("Season", 0, "day")
     let matches = this.props.handler.get("Match", 0, "current matches")
@@ -128,13 +125,96 @@ export default class Division extends React.Component {
       inds.push(x)
     }
     return (
+      <div className = {"matchesBox"}>
+        <div className = {"divisionsOrigin"}>
+          <div className = {"divisionsTopBar"}>
+            <div className = {"seasonYear"}>Season: {season}</div>
+            <div className = {"roundNumber"}>{day[0]} Round : {day[1]}</div>
+            <div className = {"clock"}>
+              {this.renderClock(this.state.matchTime)}
+            </div>
+          </div>
+          <div>
+            {(day[0] === "League") ? this.renderDivisions(matches, inds, day) : this.renderCup(matches, inds, day)}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  renderSwitch(colors) {
+    let humanTeam = this.handler.get("Human", 1, "team")
+    let players = this.handler.get("Team", humanTeam, "players")
+    let starting = players.filter((e) => {return this.handler.get("Player", e, "situation")[0] === 1})
+    let reserves = players.filter((e) => {return this.handler.get("Player", e, "situation")[0] === 2})
+    return (
+      <div className = {"matchesBox"}>
+        <div className = {"playersListSwitch"}>
+          {starting.map((e) => {
+            return (
+              <button className = {"selectPlayerButton"} onClick = {() => {this.selectPlayer(e)}} style = {colors} key = {e}>
+                {this.handler.get("Player", e, "name")} : {(this.state.switchChoices[0] === e) ? "Out" : ""}
+              </button>
+            )
+          })}
+        </div>
+        <div className = {"playersListSwitch"}>
+          {reserves.map((e) => {
+            return (
+              <button className = {"selectPlayerButton"} onClick = {() => {this.selectPlayer(e)}} style = {colors} key = {e}>
+                {this.handler.get("Player", e, "name")} : {(this.state.switchChoices[1] === e) ? "In" : ""}
+              </button>
+            )
+          })}
+        </div>
+        <button className = {"switchPlayerButton"} onClick = {() => {this.switchPlayers()}} style = {colors}>
+          Switch Players
+        </button>
+      </div>
+    )
+  }
+
+  selectPlayer(id) {
+    let sit = this.handler.get("Player", id, "situation")
+    let choices = this.state.switchChoices.slice()
+    if (sit[0] === 1) {
+      choices[0] = id
+      this.setState({switchChoices: choices})
+    } else if (sit[0] === 2) {
+      choices[1] = id
+      this.setState({switchChoices: choices})
+    }
+  }
+
+  switchPlayers() {
+    if (this.state.switchChoices[0] === 0 || this.state.switchChoices[1] === 0) {return}
+
+    let movingOut = this.state.switchChoices[0]
+    let movingOutSit = this.handler.get("Player", movingOut, "situation")
+    movingOutSit[0] = 2
+    this.handler.set("Player", movingOut, "situation", movingOutSit)
+
+    let movingIn = this.state.switchChoices[1]
+    let movingInSit = this.handler.get("Player", movingIn, "situation")
+    movingInSit[0] = 1
+    this.handler.set("Player", movingIn, "situation", movingInSit)
+
+    this.setState({switchChoices: [0, 0]})
+  }
+  
+  render(){
+    let colors = {
+      background: this.props.buttonColors[0],
+      color: this.props.buttonColors[1],
+    }
+    return (
       <>
         <div className = {"matchesTopMenu"}>
           <button 
             className = {"finishButton"} 
             style = {colors}
             onClick = {() => this.matchesButtonFunc()} 
-            disabled = {(this.state.matchesStarted === 1) ? true : false}
+            disabled = {(this.state.matchesStarted === 1 || this.state.screen !== "Matches") ? true : false}
             > 
             {(this.state.matchesStarted === 0) ? "Start" : "Finish"} Half {this.state.matchHalf}
           </button>
@@ -147,21 +227,8 @@ export default class Division extends React.Component {
           </button>
         </div>
         <div className = {"matchesBoard"}>
-          <div className = {"matchesBox"}>
-            <div className = {"divisionsOrigin"}>
-              <div className = {"divisionsTopBar"}>
-              <div className = {"seasonYear"}>Season: {season}</div>
-              <div className = {"roundNumber"}>{day[0]} Round : {day[1]}</div>
-              <div className = {"clock"}>
-                {this.renderClock(this.state.matchTime)}
-              </div>
-            </div>
-            <div>
-              {(day[0] === "League") ? this.renderDivisions(matches, inds, day) : this.renderCup(matches, inds, day)}
-            </div>
-          </div>
+          {(this.state.screen === "Matches") ? this.renderMatches() : this.renderSwitch(colors)}
         </div>
-      </div>
       </>
     );
   }
